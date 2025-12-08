dec270db55f1c9e3c831f4664dbb37ee269f1b6a
kamrulmiazi <kamrul116522@gmail.com>
2025-12-09 00:10:48 +0600

    jwt and role base security complete

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 2000;
 
 // middleware
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 dotenv.config();
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
 app.use(async (req, res, next) => {
   console.log(
     `🔰Now api call ${req.host} 🌐from ${
@@ -18,6 +21,36 @@ app.use(async (req, res, next) => {
   );
   next();
 });
+
+// verify token
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized access" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Invalid token" });
+  }
+};
+
+// verify role
+const verifyRoll = (...allowsRole) => {
+  return (req, res, next) => {
+    console.log(allowsRole);
+    if (!allowsRole.includes(req.user.role)) {
+      return res.status(403).json({
+        message: `you are forbidden user. not access for ${allowsRole}`,
+      });
+    }
+    next();
+  };
+};
 
 // connect mongodb
 const client = new MongoClient(process.env.MONGODB_URI);
@@ -60,21 +93,44 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const checkUser = await usersCollection.findOne({ email });
+// create token use function
+const createToken = (user) => {
+  return jwt.sign(
+    {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+};
 
-    if (!checkUser) {
+// user login
+app.post("/login", async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await usersCollection.findOne({ email });
+
+    if (!user) {
       return res.status(401).json({
         message: "User Not find",
       });
     }
 
-    // passwordMatch
-    const passwordMatch = await bcrypt.compare(password, checkUser.password);
+    // token create
+    const token = createToken(user);
 
-    console.log(passwordMatch);
+    // set cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "lax",
+    });
+
+    res.status(200).json({
+      message: "Login successful",
+    });
   } catch (error) {
     console.log("user login post api problem.", error);
     res.status(500).json({
@@ -84,6 +140,17 @@ app.post("/login", async (req, res) => {
   }
 });
 
+// logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+  });
+
+  res.json({ message: "Logged out successfully" });
+});
+
 // basic
 app.get("/", (req, res) => {
   return res.json({
@@ -91,6 +158,20 @@ app.get("/", (req, res) => {
     message: "Server is running now",
   });
 });
+
+// check profuse
+app.get("/check-cookie", (req, res) => {
+  res.json(req.cookies);
+});
+
+app.get(
+  "/check-roll",
+  verifyToken,
+  verifyRoll("admin", "buyer"),
+  (req, res) => {
+    res.json(req.user.role);
+  }
+);
 
 // not found page
 app.get(/.*/, (req, res) => {
