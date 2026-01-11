6d8b10d33acf63a769745cb95af448884eac662a
kamrulmiazi <kamrul116522@gmail.com>
2026-01-12 05:07:28 +0600

    update order status stats api and fixed approved data

--- index.js
+++ index.js
@@ -828,57 +828,68 @@ app.get(
 );
 
 // --------rechart data------------
-app.get("/order-stats", verifyToken, async (req, res) => {
-  console.log(req.user);
-  const { role, email } = req.user;
-  const query = {};
-  if (role === "manager") {
-    query.orderStatus = "Delivered";
-    query.managerEmail = email;
-  } else if (role === "admin") {
-    query.orderStatus = "Delivered";
-  }
-  try {
-    const getOrderStats = await ordersCollection
-      .aggregate([
-        {
-          // convert createdAt string → Date
-          $addFields: {
-            createdAtDate: { $toDate: "$createdAt" },
-          },
-        },
-        {
-          $match: query,
-        },
-        {
-          $group: {
-            _id: {
-              $dateToString: { format: "%Y-%m-%d", date: "$createdAtDate" },
-            },
-            totalOrders: {
-              $sum: 1,
-            },
-            revenue: {
-              $sum: "$totalPrice",
+
+// order total and revenue
+app.get(
+  "/order-stats",
+  verifyToken,
+  verifyRoll("admin", "manager", "buyer"),
+  async (req, res) => {
+    console.log(req.user);
+    const { role, email } = req.user;
+    const query = {};
+    if (role === "manager") {
+      query.orderStatus = "Delivered";
+      query.managerEmail = email;
+    } else if (role === "admin") {
+      query.orderStatus = "Delivered";
+    } else if (role === "buyer") {
+      query.orderStatus = "Delivered";
+      query["customer.buyerEmail"] = email;
+    }
+    try {
+      const getOrderStats = await ordersCollection
+        .aggregate([
+          {
+            // convert createdAt string → Date
+            $addFields: {
+              createdAtDate: { $toDate: "$createdAt" },
             },
           },
-        },
-        {
-          $sort: { _id: 1 },
-        },
-      ])
-      .toArray();
+          {
+            $match: query,
+          },
+          {
+            $group: {
+              _id: {
+                $dateToString: { format: "%Y-%m-%d", date: "$createdAtDate" },
+              },
+              totalOrders: {
+                $sum: 1,
+              },
+              revenue: {
+                $sum: "$totalPrice",
+              },
+            },
+          },
+          {
+            $sort: { _id: 1 },
+          },
+        ])
+        .toArray();
 
-    res.json(getOrderStats);
-  } catch (error) {
-    console.log("rechart orders stats get api problem.", error);
-    res.status(500).json({
-      status: 500,
-      message: "rechart orders stats get api some problem.",
-    });
+      res.json(getOrderStats);
+    } catch (error) {
+      console.log("rechart orders stats get api problem.", error);
+      res.status(500).json({
+        status: 500,
+        message: "rechart orders stats get api some problem.",
+      });
+    }
   }
-});
+);
 
+// order status
 app.get(
   "/orderStatus-stats",
   verifyToken,
@@ -886,11 +897,7 @@ app.get(
   async (req, res) => {
     try {
       const { role, email } = req.user;
-      const query = {
-        orderStatus: {
-          $in: ["pending", "approved", "Delivered", "rejected"],
-        },
-      };
+      const query = {};
       if (role === "manager") {
         query.managerEmail = email;
       } else if (role === "buyer") {
@@ -917,12 +924,78 @@ app.get(
           },
         ])
         .toArray();
-      res.json(getOrderStatusStats);
+
+      const STATUS_MAP = {
+        pending: ["pending"],
+        approved: [
+          "approved",
+          "packed",
+          "picked",
+          "shipped",
+          "out for delivery",
+          "in transit",
+        ],
+        delivered: ["delivered"],
+        rejected: ["rejected"],
+      };
+
+      const normalize = getOrderStatusStats.map((item) => {
+        return {
+          status: item.status.toLowerCase(),
+          orders: item?.orders,
+        };
+      });
+
+      const finalData = Object.keys(STATUS_MAP).map((finalStatus) => {
+        const relatedStatuses = STATUS_MAP[finalStatus];
+
+        const totalOrders = normalize
+          .filter((item) => relatedStatuses.includes(item.status))
+          .reduce((per, next) => per + next.orders, 0);
+
+        return {
+          status: finalStatus,
+          orders: totalOrders,
+        };
+      });
+      console.log(finalData);
+      res.json(finalData);
     } catch (error) {
       console.log("rechart order status stats get api problem.", error);
       res.status(500).json({
         status: 500,
         message: "rechart orders status stats get api some problem.",
+      });
+    }
+  }
+);
+
+// buyer total spend money
+app.get(
+  "buyer-spend-money",
+  verifyToken,
+  verifyRoll("buyer"),
+  async (req, res) => {
+    try {
+      const { email } = req.user;
+      const getSpendMoney = await ordersCollection
+        .aggregate([
+          {
+            $match: { "customer.buyerEmail": email },
+          },
+          {
+            $group: {
+              _id: { $toDate: "$createdAt" },
+            },
+          },
+        ])
+        .toArray();
+      res.json(getSpendMoney);
+    } catch (error) {
+      console.log("rechart buyer spend money get api problem.", error);
+      res.status(500).json({
+        status: 500,
+        message: "rechart buyer spend money get api some problem.",
       });
     }
   }
